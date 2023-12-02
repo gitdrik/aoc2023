f7b37565c692ccfca58250d055cf96698fd6d474
gitdrik <58667795+gitdrik@users.noreply.github.com>
2023-12-02 17:44:33 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,6 +1,5 @@
 open("02.txt") do f
     p1, p2 = 0, 0
-    bmax = [12, 13, 14]
     ci = Dict('r'=>1, 'g'=>2, 'b'=>3)
     for (g, l) ∈ enumerate(eachline(f))
         bmin = [0, 0, 0]
@@ -9,7 +8,7 @@ open("02.txt") do f
             n, c = parse(Int, ss[i]), ss[i+1][1]
             bmin[ci[c]] = max(n, bmin[ci[c]])
         end
-        all(bmin .≤ bmax) && (p1 += g)
+        all(bmin .≤ [12, 13, 14]) && (p1 += g)
         p2 += prod(bmin)
     end
     println("Part 1: ", p1)
