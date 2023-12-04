d9d783be261784fdd93064fc271aab0fb3ff9558
gitdrik <58667795+gitdrik@users.noreply.github.com>
2023-12-04 07:52:02 +0100

    Add files via upload

--- 04.jl
+++ 04.jl
@@ -1,15 +1,14 @@
 open("04.txt") do f
     p1 = 0
-    C = fill(1, 204, 2)
+    C = fill(1, 204)
     for (i, l) ∈ enumerate(eachline(f))
         ss = split(l[11:end])
         m = length(ss[1:10] ∩ ss[12:end])
-        C[i, 1] = m
         if m > 0
             p1 += 2^(m-1)
-            C[i+1:i+m, 2] .+= C[i,2]
+            C[i+1:i+m] .+= C[i]
         end
     end
     println("Part 1: ", p1)
-    println("Part 2: ", sum(C[:,2]))
+    println("Part 2: ", sum(C))
 end
