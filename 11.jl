2d65e3656903a3dc3f8831a7df608cb0c4657373
gitdrik <58667795+gitdrik@users.noreply.github.com>
2023-12-11 08:34:26 +0100

    Add files via upload

--- 11.jl
+++ 11.jl
@@ -18,17 +18,17 @@ open("11.txt") do f
     end
     EC = [c for c ∈ 1:cols if c ∉ notEC]
 
-    function dist(p2)
+    function dist(expansion)
         d = 0
         for i ∈ 1:length(G)-1, j ∈ i+1:length(G)
             r1, r2 = sort([G[i][1], G[j][1]])
             c1, c2 = sort([G[i][2], G[j][2]])
             d += r2+c2-r1-c1
-            d += (1 + p2*999998) * sum(r ∈ r1:r2 for r∈ER)
-            d += (1 + p2*999998) * sum(c ∈ c1:c2 for c∈EC)
+            d += expansion * sum(r ∈ r1:r2 for r∈ER)
+            d += expansion * sum(c ∈ c1:c2 for c∈EC)
         end
         return d
     end
-    println("Part 1: ", dist(false))
-    println("Part 2: ", dist(true))
+    println("Part 1: ", dist(1))
+    println("Part 2: ", dist(999999))
 end
