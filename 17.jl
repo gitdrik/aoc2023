e6c432efc0488a62c3b7caf64566bdd3b8685811
gitdrik <58667795+gitdrik@users.noreply.github.com>
2023-12-17 13:09:14 +0100

    Add files via upload

---
+++ 17.jl
@@ -0,0 +1,29 @@
+using DataStructures
+
+open("17.txt") do f
+    M = [parse.(Int, collect(l)) for l ∈ eachline(f)]
+    R = length(M)
+    C = length(M[1])
+
+    function solve(minstep, maxstep)
+        seen::Dict{Tuple{Int,Int,Int,Int}, Int} = Dict()
+        dp = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        Q = SortedSet([(0, 1, 1, 3, 0)])
+        while !isempty(Q)
+            h, r, c, d, steps = pop!(Q)
+            (r, c)==(R, C) && return h
+            (r, c, d, steps) ∈ keys(seen) && seen[(r, c, d, steps)] ≤ h && continue
+            seen[(r, c, d, steps)] = h
+            for nd ∈ d-1:d+1
+                nd = mod1(nd, 4)
+                nd==d && steps==maxstep && continue
+                nd≠d && steps < minstep && continue
+                nsteps = nd≠d ? 0 : steps
+                nr, nc = (r, c) .+ dp[nd]
+                nr∈1:R && nc∈1:C && push!(Q, (h+M[nr][nc], nr, nc, nd, nsteps+1))
+            end
+        end
+    end
+    println("Part 1: ", solve(1, 3))
+    println("Part 2: ", solve(4, 10))
+end
